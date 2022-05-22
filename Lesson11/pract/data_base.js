672464593dd359c7ef9c3451ca5a3ce501784cf9
Юрій Середюк <46564034+yura-seredyuk@users.noreply.github.com>
2022-05-22 21:36:00 +0300

    Delete Lesson11 directory

--- Lesson11/pract/data_base.js
+++
@@ -1,6 +0,0 @@
-let data_base = [
-    ["1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos consequuntur, ratione excepturi quos ad placeat ullam libero eaque blanditiis neque. Aliquam iusto fugit error voluptas repudiandae soluta molestias corrupti itaque?"],
-    ["2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos consequuntur, ratione excepturi quos ad placeat ullam libero eaque blanditiis neque. Aliquam iusto fugit error voluptas repudiandae soluta molestias corrupti itaque?"]
-];
-
-export {data_base};
