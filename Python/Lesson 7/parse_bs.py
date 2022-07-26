b1f310af83b2a1d3621427191ba0e69b31c07c76
Yurii Serediuk <yurii.seredyuk@gmail.com>
2022-07-26 20:11:32 +0300

    python: scrapping/parser

--- Python/Lesson 7/parse_bs.py
+++ Python/Lesson 7/parse_bs.py
@@ -33,14 +33,18 @@ for item in links[::-1]:
     if item.text.strip().isdigit():
         last_page = int(item.text.strip())
         break
-
+i = 0
 for page in range(1,last_page+1):
     newUrl = f'{URL}/exercise/list_basic/?page={page}'
     response = requests.get(newUrl)
     soup = BeautifulSoup(response.text, 'lxml')
     items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
-    for i, item in enumerate(items, start=1):
-        itemName = item.find('h4', class_="card-title").text.strip()
-        itemPrice = item.find('h5').text
-        itemImage = item.find('img').get('src')
-        print(f'Page {page} {i}: {itemPrice} -- {itemName} ({URL+itemImage})')
+    for i, item in enumerate(items, start=i+1):
+        itemLink = item.select_one('h4 a').get("href")
+        response = requests.get(URL+itemLink)
+        soup_item = BeautifulSoup(response.text, 'lxml')
+        itemName = soup_item.find('h3', class_="card-title").text.strip()
+        itemPrice = soup_item.find('h4').text
+        itemImage = soup_item.find('img', class_="card-img-top img-fluid").get('src')
+        itemDetails = soup_item.find('p',class_="card-text").text.strip()
+        print(f'Page {page} {i}: {itemPrice} -- {itemName} \n\t {itemDetails} \n\t ({URL+itemImage}), {URL+itemLink}')
