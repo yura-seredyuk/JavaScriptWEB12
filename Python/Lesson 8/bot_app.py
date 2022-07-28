3e50c3255aae9c26f946193e1a7c50947c214f6a
Yurii Serediuk <yurii.seredyuk@gmail.com>
2022-07-28 19:55:19 +0300

    Python: telegram bot - base commands

--- Python/Lesson 8/bot_app.py
+++ Python/Lesson 8/bot_app.py
@@ -18,10 +18,11 @@ def search(message):
 
 @bot.message_handler(commands=['buttons'])
 def buttons(message):
-    markup = telebot.types.InlineKeyboardMarkup()
+    markup = telebot.types.InlineKeyboardMarkup() 
     markup.add(telebot.types.InlineKeyboardButton(text='Btn1', callback_data=1))
     markup.add(telebot.types.InlineKeyboardButton(text='Btn2', callback_data=2))
-    markup.add(telebot.types.InlineKeyboardButton(text='Btn3', callback_data=3))
+    markup.add(telebot.types.InlineKeyboardButton(text='Btn3', callback_data=3),
+                telebot.types.InlineKeyboardButton(text='Btn4', callback_data=4))
     bot.send_message(message.chat.id, "Show buttons!", reply_markup = markup)
 
 
@@ -35,9 +36,19 @@ def query_handler(call):
         answer = 'It was 2!'
     elif call.data == '3':
         answer = 'It was 3!'
-    bot.send_message(call.message.chat.id, answer)
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id) # hide buttons
+    elif call.data == '4':
+        answer = 'It was 4!'
+    bot.send_message(call.message.chat.id, answer) 
+    # bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id) # hide buttons
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() in ['hello', 'hi']:
+        bot.send_message(message.chat.id, "Hello!")
+    elif message.text.lower() in ['bye', 'goodbye']:
+        bot.send_message(message.chat.id, "Goodbye!")
+    else:
+        bot.send_message(message.chat.id, "I dont understand!!!")
 
 
 
